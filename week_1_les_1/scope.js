3bd55cea1525b8a21ef879aa3a9b175708b510ac
Pgm-Hasan <Khasan.ouzoun@student.arteveldehs.be>
2025-11-19 15:11:39 +0100

    Explained Scope

---
+++ week_1_les_1/scope.js
@@ -0,0 +1,12 @@
+const message = 'Hey daar!';
+
+function say() {
+    const newMessage = 'Yolo';
+    console.log(message)
+}
+
+console.log(newMesseage)
+say();
+
+sayHallo();
+console.log(message);
